78d3ce852b88936e79802c21c8c1a1ad9669c8c4
Matamenas <B00149694@mytudublin.ie>
2023-10-10 20:10:56 +0100

    upload #301

---
+++ Assets/Challenge 2/Scripts/LinkX.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkX : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Openlink()
+    {
+        Application.OpenURL("https://github.com/Matamenas/Unit2_B00149694");
+    }
+}
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float sendSpeed = 1.5f;
+    private float nextSend = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSend)
+        {
+            nextSend = Time.time + sendSpeed;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
